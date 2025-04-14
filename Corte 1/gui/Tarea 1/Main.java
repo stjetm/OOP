91321d9e53fec93220143096a0c2dc71ad91ce8d
stjetm <jaetorresm@udistrital.edu.co>
2025-04-13 22:53:38 -0500

    Update Main.java

--- Corte 1/gui/Tarea 1/Main.java
+++ Corte 1/gui/Tarea 1/Main.java
@@ -1,1 +1,19 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+        Operations resultado = new Operations();
+        resultado.sumar(3, 2);
+        System.out.println(resultado.r);
+        
+        resultado.restar(4, 6);
+        System.out.println(resultado.r);
+        
+        resultado.multiplicar(6, 7);
+        System.out.println(resultado.r);
+        
+        resultado.potencia(2, 3);
+        System.out.println(resultado.r);
+    }
+    
 
