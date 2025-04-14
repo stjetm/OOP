438f6063bdae1fcf9c0e9a5eaa16b2bce31c1340
stjetm <jaetorresm@udistrital.edu.co>
2025-04-13 22:53:05 -0500

    Update Operations.java

--- Corte 1/gui/Tarea 1/Operations.java
+++ Corte 1/gui/Tarea 1/Operations.java
@@ -1,1 +1,21 @@
+public class Operations {
+    
+    public int a,b,r; 
+    public int sumar(int a, int b){
+        r = a + b;
+        return r;
+    }
+    public int restar(int a, int b){
+        r = a - b;
+        return r;
+    }
+    public int multiplicar(int a, int b){
+        r = a * b;
+        return r;
+    }
+    public int potencia(int base, int exponente){
+        r = exponente == 0 ? 1: base*potencia(base,exponente-1);
+        return r;
+    }
+}
 
