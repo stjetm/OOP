5eba5394b172b9bc86915ec15bc3412c0410eb6d
stjetm <jaetorresm@udistrital.edu.co>
2025-05-13 17:16:13 -0500

    Update CuentaBancaria.java

--- Corte 1/gui/Evaluación1/CuentaBancaria.java
+++ Corte 1/gui/Evaluación1/CuentaBancaria.java
@@ -6,29 +6,12 @@ public class CuentaBancaria {
         this.saldo = 0.0;
     }
 
-    // Getter para saldo
     public double getSaldo() {
         return saldo;
     }
 
-    // Setter para saldo (aunque no se usará directamente)
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
-
-    // Método para depositar dinero
-    public void depositar(double monto) {
-        if (monto > 0) {
-            setSaldo(getSaldo() + monto); // Usamos getters y setters
-        }
-    }
-
-    // Método para retirar dinero
-    public boolean retirar(double monto) {
-        if (monto > 0 && monto <= getSaldo()) {
-            setSaldo(getSaldo() - monto); // Usamos getters y setters
-            return true;
-        }
-        return false;
-    }
 }
+
