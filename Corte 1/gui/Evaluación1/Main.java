9401784b67677c32b60a6cae86b287dc0df7dfbc
stjetm <jaetorresm@udistrital.edu.co>
2025-05-13 17:07:01 -0500

    Create Main.java

---
+++ Corte 1/gui/Evaluación1/Main.java
@@ -0,0 +1,10 @@
+package Cajero;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new CajeroGUI());
+    }
+}
+
